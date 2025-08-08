caf39446d98b7a0fe4430971f61ca9844a6f981d
Jesus Avina Alcala <alcalajesus846@gmail.com>
2025-08-08 10:20:08 -0600

    .

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,5 +1,9 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 export const experimental_ppr = true;
+import { Metadata } from 'next';
+ 
+
+
 export default function Layout ({children}){
     return (
   <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
