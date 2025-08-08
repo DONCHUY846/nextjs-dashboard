3ddef2e19d5825e73947ba9f195a3c0b6b342dd7
Jesus Avina Alcala <alcalajesus846@gmail.com>
2025-08-08 09:48:31 -0600

    nextauth

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,15 +9,13 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
-export default function EditInvoiceForm({
-  invoice,
-  customers,
-}: {
-  invoice: InvoiceForm;
-  customers: CustomerField[];
-}) {
+import { updateInvoice , State } from '@/app/lib/actions';
+import { useActionState } from 'react';
+
+export default function EditInvoiceForm({  invoice,  customers,}: {  invoice: InvoiceForm;  customers: CustomerField[];}) {
+   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
  
   return (
     <form>
